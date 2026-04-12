59f37326fd507455fd5e079ba45e9f69b66da9f2
Hitesh Mehra <mehrahitesh2006@gmail.com>
2026-04-12 12:34:42 +0530

    deploying mvp changing browser2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import ColorGrid from "./components/ColorGrid.jsx";
 import RootLayout from "./pages/RootLayout.jsx";
@@ -11,7 +11,7 @@ import SavesPalette from "./pages/SavesPalette.jsx";
 import { PalettesProvider } from "./context/Palettes.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <PalettesProvider>
       <Routes>
         <Route path="/" element={<RootLayout />}>
@@ -22,5 +22,5 @@ createRoot(document.getElementById("root")).render(
         </Route>
       </Routes>
     </PalettesProvider>
-  </HashRouter>,
+  </BrowserRouter>,
 );
